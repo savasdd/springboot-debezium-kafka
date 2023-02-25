7bccb43d58f00826b114960ffa4a472ef07542f0
Savaş Dede <svsdd22@gmail.com>
2023-02-25 23:10:17 +0300

    debezium config

--- DebeziumTarget/src/main/java/com/debezium/config/DebeziumConnectorConfig.java
+++ DebeziumTarget/src/main/java/com/debezium/config/DebeziumConnectorConfig.java
@@ -10,42 +10,42 @@ import java.io.IOException;
 @Configuration
 public class DebeziumConnectorConfig {
 
-    @Value("${customer.datasource.host}")
-    private String customerDbHost;
+    @Value("${debezium.datasource.host}")
+    private String sourceDbHost;
 
-    @Value("${customer.datasource.database}")
-    private String customerDbName;
+    @Value("${debezium.datasource.database}")
+    private String sourceDbName;
 
-    @Value("${customer.datasource.port}")
-    private String customerDbPort;
+    @Value("${debezium.datasource.port}")
+    private String sourceDbPort;
 
-    @Value("${customer.datasource.username}")
-    private String customerDbUsername;
+    @Value("${debezium.datasource.username}")
+    private String sourceDbUsername;
 
-    @Value("${customer.datasource.password}")
-    private String customerDbPassword;
+    @Value("${debezium.datasource.password}")
+    private String sourceDbPassword;
 
     @Bean
     public io.debezium.config.Configuration customerConnector() throws IOException {
         File offsetStorageTempFile = File.createTempFile("offsets_", ".dat");
         File dbHistoryTempFile = File.createTempFile("dbhistory_", ".dat");
         return io.debezium.config.Configuration.create()
-                .with("name", "customer-postgres-connector")
+                .with("name", "source-postgres-connector")
                 .with("connector.class", "io.debezium.connector.postgresql.PostgresConnector")
                 .with("offset.storage", "org.apache.kafka.connect.storage.FileOffsetBackingStore")
                 .with("offset.storage.file.filename", offsetStorageTempFile.getAbsolutePath())
                 .with("offset.flush.interval.ms", "60000")
-                .with("database.hostname", customerDbHost)
-                .with("database.port", customerDbPort)
+                .with("database.hostname", sourceDbHost)
+                .with("database.port", sourceDbPort)
                 .with("plugin.name", "pgoutput")
-                .with("database.user", customerDbUsername)
-                .with("database.password", customerDbPassword)
-                .with("database.dbname", customerDbName)
-                .with("database.include.list", customerDbName)
+                .with("database.user", sourceDbUsername)
+                .with("database.password", sourceDbPassword)
+                .with("database.dbname", sourceDbName)
+                .with("database.include.list", sourceDbName)
                 .with("include.schema.changes", "false")
                 .with("database.allowPublicKeyRetrieval", "true")
                 .with("database.server.id", "10181")
-                .with("database.server.name", "customer-postgres-db-server")
+                .with("database.server.name", "source-postgres-db-server")
                 .with("database.history", "io.debezium.relational.history.FileDatabaseHistory")
                 .with("database.history.file.filename", dbHistoryTempFile.getAbsolutePath())
                 .build();
